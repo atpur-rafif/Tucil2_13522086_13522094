addd4cff569f626c06c846f8902f3fdaec7846f2
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-03-18 16:11:59 +0700

    fix: fixed bruteforce iteration

--- src/client/module/bezier/bruteForce.ts
+++ src/client/module/bezier/bruteForce.ts
@@ -7,25 +7,25 @@ export class Calculator {
 	private isVisited: number[] = [];
 	private nthfactorial: number[] = [];
 	factorial(n: number) {
-		if(!n) {
+		if (!n) {
 			return 1;
 		}
-		else if(this.isVisited[n]) {
+		else if (this.isVisited[n]) {
 			return this.nthfactorial[n];
 		}
 		else {
 			this.isVisited[n] = 1;
-			this.nthfactorial[n] = n * this.factorial(n-1);
+			this.nthfactorial[n] = n * this.factorial(n - 1);
 			return this.nthfactorial[n];
 		}
 	}
 
 	combination(n: number, k: number) {
-		return (this.factorial(n)/(this.factorial(k) * this.factorial(n-k)));
+		return (this.factorial(n) / (this.factorial(k) * this.factorial(n - k)));
 	}
-	
+
 	bernsteinPolynomial(k: number, n: number, u: number) {
-		return ((this.combination(n, k)) * (u**k) * ((1-u)**(n-k)));
+		return ((this.combination(n, k)) * (u ** k) * ((1 - u) ** (n - k)));
 	}
 }
 export class BezierBF {
@@ -35,10 +35,10 @@ export class BezierBF {
 		this.lazyPoint = new LazyPoint(point);
 	}
 
-	point(k: number, n: number, u: number) {
-		const p: Point = this.lazyPoint.control[k];
+	point(lp: LazyPoint, k: number, n: number, u: number) {
+		const p: Point = lp.control[k];
 		p.x *= this.calc.bernsteinPolynomial(k, n, u);
-		p.y *= this.calc.bernsteinPolynomial(k, n, u)
+		p.y *= this.calc.bernsteinPolynomial(k, n, u);
 		return p;
 	}
 
@@ -49,10 +49,14 @@ export class BezierBF {
 	*/
 	solver(n: number, iteration: number, lazy: LazyPoint) {
 		const accumulator: Point[] = [];
-		for(let k = 0; k <= n; k++) {
-			for(let u = 0; u <= iteration; u++) {
-				accumulator.push(this.point(k, n, u/iteration));
+		for (let u = 0; u <= iteration; u++) {
+			const sum: Point = { x: 0, y: 0 };
+			for (let k = 0; k <= n; k++) {
+				let temp: Point = this.point(lazy, k, n, u / iteration);
+				sum.x += temp.x;
+				sum.y += temp.y;
 			}
+			accumulator.push(sum);
 		}
 		return accumulator;
 	}
